38b9aab8c3c0f82d26f982ff63588251cd9afaeb
Hector14oo <devtor140017@gmail.com>
2023-08-10 15:57:55 -0500

    :tada: Initial commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,26 @@
+# PULL REQUEST 📋
+
+## ▶ Descripción
+
+> Completa con el detalle del cambio
+
+## ▶ Información para el Reviewer
+
+> Comenta el flujo que se tiene que realizar para poder testear el nuevo cambio
+
+## 🔑 Datos de usuario para probar el cambio:
+
+| UserID | Username | Password | SiteID |
+| ------ | -------- | -------- | ------ |
+|        |          |          |        |
+|        |          |          |        |
+|        |          |          |        |
+|        |          |          |        |
+
+## 🏷 Tipo de PR
+
+- [ ] Bugfix
+- [ ] Feature
+- [ ] Refactoring
+- [ ] Update de dependencias
+- [ ] Otro (describir):
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      // Primary
+
+      'Marine-blue': '#02295a',
+      'Purplish-blue': '#473dff',
+      'Pastel-blue': '#adbeff',
+      'Light-blue': '#bfe2fd',
+      'Strawberry-red': '#ed3548',
+
+      // Neutral
+
+      'Cool-gray': '#9699ab',
+      'Light-gray': '#d6d9e6',
+      Magnolia: '#f0f6ff',
+      Alabaster: '#fafbff',
+      White: '#ffffff',
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
