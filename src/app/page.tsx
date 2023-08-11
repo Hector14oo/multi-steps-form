b139271a8a4612b2cae465eb0a08a22e58070b7a
Hector14oo <devtor140017@gmail.com>
2023-08-11 16:10:56 -0500

    :construction: Page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
+import { Form } from '@/components/Form';
+import { ScreenOne } from '@/components/FormScreens';
+
 import { STEP_LIST } from '@/utils/constants';
 
-import { StepOne } from '@/components/Form';
-
 export default function Home() {
+  const page = 1;
   return (
     <main className='w-full h-full flex flex-col items-center gap-4 bg-[url("/images/bg-sidebar-mobile.svg")] bg-no-repeat'>
       <section>
         <ol className='mt-8 mb-[18px] flex justify-center align-middle gap-8'>
           {STEP_LIST.map((step, i) => {
             return (
-              <li key={step.label} className={`w-8 h-8 flex justify-center items-center ${i === 0 ? 'bg-Light-blue  text-Marine-blue' : ' text-White border-White border'}  rounded-full`}>
+              <li
+                key={step.label}
+                className={`w-8 h-8 flex justify-center items-center ${i === 0 ? 'bg-Light-blue  text-Marine-blue' : ' text-White border-White border'}  rounded-full`}
+              >
                 {step.value}
               </li>
             );
           })}
         </ol>
       </section>
-
-      {true && <StepOne />}
+      {page === 1 && (
+        <Form
+          legend='Personal info'
+          description='Please provide your name, email address, and phone number.'
+          page={page}
+        >
+          <ScreenOne />
+        </Form>
+      )}
     </main>
   );
 }
