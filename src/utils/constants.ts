e10b9bbd2056aeb4a2f93d3fdfd31115953ba3a8
Hector14oo <devtor140017@gmail.com>
2023-08-11 14:22:50 -0500

    :truck: Changed variable names4

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
-export const INPUTS = [
+export const INPUT_LIST = [
   { label: 'Name', type: 'text', name:'name', placeholder: 'Stephen King' },
   { label: 'Email Address', type: 'email', name:'email', placeholder: 'stephenking@lorem.com' },
   { label: 'Phone Number', type: 'number', name:'number', placeholder: '+57 313 111 1111' },
 ];
 
-export const STEPS = [
+export const STEP_LIST = [
   { value: '1', label: 'STEP 1', description: 'YOUR INFO' },
   { value: '2', label: 'STEP 2', description: 'SELECT PLAN' },
   { value: '3', label: 'STEP 3', description: 'ADD-ONS' },
