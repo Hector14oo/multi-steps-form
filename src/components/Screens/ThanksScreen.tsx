7bd6c5c3721c3468ba7687bf7a3904fb37a4e86f
Hector14oo <devtor140017@gmail.com>
2023-08-17 13:53:29 -0500

    :sparkles; ThanksScreen added

---
+++ src/components/Screens/ThanksScreen.tsx
@@ -0,0 +1,19 @@
+import { ThanksSVG } from '@public/images/ThanksSVG';
+
+export function ThanksScreen() {
+  return (
+    <section className='p-6 h-96 flex flex-col justify-center items-center text-center gap-3 w-11/12 bg-White rounded-xl shadow-[0_20px_25px_-5px_rgba(0,0,0,0.1)]'>
+      <i>
+        <ThanksSVG />
+      </i>
+      <h1 className='mt-2 text-2xl font-bold text-Marine-blue'>Thank you!</h1>
+      <p className='mb-2 font-light text-Cool-gray'>
+        Thanks for confirming your subscription!
+        We hope you have fun using our
+        platform. If you ever need support,
+        please fell free to email us at 
+        support@loremgaming.com.
+      </p>
+    </section>
+  );
+}
