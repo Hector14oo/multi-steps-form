01fa4a837ad42fa09e1b8ded144e29e70dd2f644
Hector14oo <devtor140017@gmail.com>
2023-08-17 16:30:46 -0500

    :lipstick: ScreenThree styles updated

--- src/components/Screens/ScreenThree.tsx
+++ src/components/Screens/ScreenThree.tsx
@@ -8,20 +8,20 @@ export function ScreenThree() {
       {CHECKBOX_LIST(globalState.planType).map((checkbox) => (
         <label
           key={checkbox.label}
-          className='pl-3 w-full relative flex items-center gap-4'
+          className='py-2 pl-4 w-full relative flex items-center gap-4'
         >
           <input
             type='checkbox'
             name='addOns'
             value={checkbox.label}
-            className='w-6 h-6 peer after:absolute after:rounded-lg after:top-0 after:left-0 after:w-full after:h-full after:appearance-none after:ring-1 after:ring-Light-gray after:checked:ring-Marine-blue'
+            className='w-6 h-6 peer accent-Purplish-blue after:absolute after:rounded-lg after:top-0 after:left-0 after:w-full after:h-full after:appearance-none after:ring-1 after:ring-Light-gray after:checked:ring-Marine-blue after:checked:bg-Purplish-blue after:checked:bg-opacity-5'
           />
           <article className='p-3 pl-0 w-full flex items-center gap-4'>
-            <span className='w-full text-sm text-Marine-blue font-medium'>
+            <span className='w-full text-sm md:text-base text-Marine-blue font-medium'>
               {checkbox.label}
-              <span className='w-full text-xs flex justify-between items-center text-Cool-gray'>{checkbox.description}</span>
+              <span className='w-full text-xs md:text-sm flex justify-between items-center text-Cool-gray'>{checkbox.description}</span>
             </span>
-            <span className='text-xs text-Purplish-blue'>
+            <span className='text-sm md:text-base text-Purplish-blue'>
               ${checkbox.price}/{globalState.planType ? 'yr' : 'mo'}
             </span>
           </article>
