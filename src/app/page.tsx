6dc2c50a1f6c65f79487dbc5079e1c87cd6bff6b
Hector14oo <devtor140017@gmail.com>
2023-08-18 16:29:12 -0500

    :lipstick: Page styles updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const { globalState } = useGlobalContext();
   return (
     <main className='lg:p-4 w-full h-full lg:w-[1024px] lg:min-h-[512px] lg:h-auto flex flex-col lg:grid lg:grid-cols-[25%_1fr] lg:grid-rows-[650px] lg:relative overflow-hidden items-center lg:items-start gap-4 lg:bg-White lg:rounded-xl before:w-full before:h-1/5 before:absolute before:bg-[url("/images/bg-sidebar-mobile.svg")] before:bg-cover before:z-[-1] lg:before:bg-none lg:shadow-[0_20px_25px_-5px_rgba(0,0,0,0.1)]'>
-      <section className='lg:w-full lg:h-full rounded-xl overflow-hidden'>
+      <section className='lg:w-full lg:h-full rounded-xl overflow-hidden pointer-events-none'>
         <ol className='mt-8 mb-[18px] lg:m-0 lg:pt-8 lg:h-full flex lg:flex-col justify-center lg:justify-start items-center gap-8 lg:bg-[url("/images/bg-sidebar-desktop.svg")] bg-no-repeat bg-cover'>
           {STEP_LIST.map((step, i) => {
             return (
