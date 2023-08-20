8aef144fe541ef90207fdba915f8fee5252b8144
Hector14oo <devtor140017@gmail.com>
2023-08-20 18:15:17 -0500

    :lipstick: ScreenOne styles for validations

--- src/components/Screens/ScreenOne.tsx
+++ src/components/Screens/ScreenOne.tsx
@@ -11,7 +11,11 @@ export function ScreenOne() {
         type={data.type}
         name={data.name}
         placeholder={`e.g. ${data.placeholder}`}
-        className='px-3 py-[10px] text-sm md:text-lg font-medium ring-1 ring-Light-gray hover:ring-Marine-blue rounded-md '
+        className='px-3 py-[10px] relative text-sm md:text-lg font-medium ring-1 ring-Light-gray outline-Marine-blue rounded-md invalid:ring-Strawberry-red invalid:outline-Strawberry-red'
+        onChange={(e) => {
+          e.target.classList.replace('ring-Strawberry-red', 'ring-Light-gray');
+          e.target.classList.replace('outline-Strawberry-red', 'outline-Marine-blue');
+        }}
       />
     </label>
   ));
