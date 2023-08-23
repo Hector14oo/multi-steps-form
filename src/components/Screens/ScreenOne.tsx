1c2eaf3e07e726c3ff510502c41e5878fb679c6d
Hector14oo <devtor140017@gmail.com>
2023-08-22 21:10:39 -0500

    :triangular_flag_on_post: Value persist removed

--- src/components/Screens/ScreenOne.tsx
+++ src/components/Screens/ScreenOne.tsx
@@ -14,7 +14,6 @@ export function ScreenOne() {
         name={data.name}
         placeholder={`e.g. ${data.placeholder}`}
         className='px-3 py-[10px] relative text-sm md:text-lg font-medium ring-1 ring-Light-gray hover:ring-Purplish-blue outline-Purplish-blue  rounded-md invalid:ring-Strawberry-red invalid:outline-Strawberry-red transition-[--tw-ring-color] ease-out duration-200'
-        value={globalState.userData[data.name as keyof typeof globalState.userData]}
         onChange={(e) => {
           e.target.classList.replace('ring-Strawberry-red', 'ring-Light-gray');
           e.target.classList.replace('outline-Strawberry-red', 'outline-Marine-blue');
